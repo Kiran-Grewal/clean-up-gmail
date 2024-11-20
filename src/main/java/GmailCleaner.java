3e0937b19978853f595528625fa4e2076c20a826
Kiran-Grewal <115323108+Kiran-Grewal@users.noreply.github.com>
2024-11-19 19:02:31 -0600

    Screenshots removed

---
+++ src/main/java/GmailCleaner.java
@@ -0,0 +1,25 @@
+import com.google.api.services.gmail.Gmail;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+public class GmailCleaner {
+
+    public static void main(String[] args) throws IOException, GeneralSecurityException{
+
+        GmailService gmailservice = new GmailService();
+        Gmail service = gmailservice.getGmailService(); //gmail API client service.
+
+        EmailTrasher emailTrasher = new EmailTrasher(service);
+
+        OldEmailFetcher oldEmailFetcher = new OldEmailFetcher(service,emailTrasher);
+        oldEmailFetcher.trashOldEmails();
+
+//        EmailBodyFetcher emailBodyFetcher = new EmailBodyFetcher(service);
+//        EmailExpiryChecker emailExpiryChecker = new EmailExpiryChecker();
+//        ExpiredCouponFetcher expiredCouponFetcher = new ExpiredCouponFetcher(service,emailTrasher,
+//                                                            emailBodyFetcher,emailExpiryChecker);
+//        expiredCouponFetcher.trashExpiredEmails();
+
+    }
+}
