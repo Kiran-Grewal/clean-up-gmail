eaa9c1304909f7ae6f23832951d80859217294cc
Kiran-Grewal <115323108+Kiran-Grewal@users.noreply.github.com>
2024-11-29 16:23:31 -0600

    Tests added to EmailBodyFetcherTest

--- src/test/java/EmailBodyFetcherTest.java
+++ src/test/java/EmailBodyFetcherTest.java
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -18,8 +20,8 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class EmailBodyFetcherTest {
     private EmailBodyFetcher emailBodyFetcher;
-    Message mockMsg;
-    String mockMsgId = "";
+    private Message mockMsg;
+    private final String mockMsgId = "";
 
     @BeforeEach
     public void setUp() throws IOException {
@@ -37,18 +39,123 @@ class EmailBodyFetcherTest {
     }
 
     @Test
-    @DisplayName("Text/Plain is the only mime type")
+    @DisplayName("When text/plain is the only mime type")
     public void getEmailBodyTest1() {
-
             MessagePart mockMsgPayload = new MessagePart();
             MessagePartBody mockBody = new MessagePartBody();
-            String mockData = "cGxhaW4gdGV4dCBwYXJ0";
+            String mockData = "dGV4dC9wbGFpbiBwYXJ0";           //base64URL encoded String for "text/plain part"
+
             mockMsgPayload.setMimeType("text/plain");
             mockBody.setData(mockData);
             mockMsgPayload.setBody(mockBody);
             mockMsg.setPayload(mockMsgPayload);
 
             String result = emailBodyFetcher.getEmailBody(mockMsgId);
-            Assertions.assertEquals(result,"plain text part");
+            Assertions.assertEquals(result,"text/plain part");
+    }
+
+    @Test
+    @DisplayName("When html/plain is the only mime type")
+    public void getEmailBodyTest2() {
+        MessagePart mockMsgPayload = new MessagePart();
+        MessagePartBody mockBody = new MessagePartBody();
+        String mockData = "aHRtbC9wbGFpbiBwYXJ0";               //base64URL encoded String for "html/plain part"
+
+        mockMsgPayload.setMimeType("html/plain");
+        mockBody.setData(mockData);
+        mockMsgPayload.setBody(mockBody);
+        mockMsg.setPayload(mockMsgPayload);
+
+        String result = emailBodyFetcher.getEmailBody(mockMsgId);
+        Assertions.assertEquals(result,"");
+    }
+
+    @Test
+    @DisplayName("When text/plain is inside multipart mime type")
+    public void getEmailBodyTest3() {
+
+        MessagePart mockMsgPayload = new MessagePart();
+        mockMsgPayload.setMimeType("multipart/alternative");
+        List<MessagePart> mockParts = new ArrayList<>();
+        MessagePart mockMsgPart = new MessagePart();      //mockMsgPart inside original mockMsgPayload
+        MessagePartBody mockBody = new MessagePartBody();
+        mockMsgPart.setMimeType("text/plain");
+        String mockData =
+              "dGV4dC9wbGFpbiBwYXJ0IGluIG11bHRpcGFydA=="; //base64URL encoded String for "text/plain part in multipart"
+
+        mockBody.setData(mockData);
+        mockMsgPart.setBody(mockBody);
+        mockParts.add(mockMsgPart);
+        mockMsgPayload.setParts(mockParts);
+        mockMsg.setPayload(mockMsgPayload);
+
+        String result = emailBodyFetcher.getEmailBody(mockMsgId);
+        Assertions.assertEquals(result,"text/plain part in multipart");
+    }
+
+    @Test
+    @DisplayName("When text/plain and html/plain are both inside multipart/mixed type")
+    public void getEmailBodyTest4() {
+
+        MessagePart mockMsgPayload = new MessagePart();
+        mockMsgPayload.setMimeType("multipart/mixed");
+        List<MessagePart> mockParts = new ArrayList<>();
+
+        //mockMsgPart1 inside original mockMsgPayload
+        MessagePart mockMsgPart1 = new MessagePart();
+        mockMsgPart1.setMimeType("html/plain");
+
+        //mockMsgPart2 inside original mockMsgPayload
+        MessagePart mockMsgPart2 = new MessagePart();
+        MessagePartBody mockBody = new MessagePartBody();
+        mockMsgPart2.setMimeType("text/plain");
+        String mockData =
+                "dGV4dC9wbGFpbiBwYXJ0IGluIG11bHRpcGFydA=="; //base64URL encoded String for "text/plain part in multipart"
+
+        mockBody.setData(mockData);
+        mockMsgPart2.setBody(mockBody);             //setting body for text/plain mockMsgPart
+
+        mockParts.add(mockMsgPart1);
+        mockParts.add(mockMsgPart2);
+        mockMsgPayload.setParts(mockParts);
+        mockMsg.setPayload(mockMsgPayload);
+
+        String result = emailBodyFetcher.getEmailBody(mockMsgId);
+        Assertions.assertEquals(result,"text/plain part in multipart");
+    }
+
+
+    @Test
+    @DisplayName("When text/plain is in multipart/alternative which is in multipart/mixed type")
+    public void getEmailBodyTest5() {
+
+        MessagePart mockMsgPayload = new MessagePart();
+        mockMsgPayload.setMimeType("multipart/mixed");
+        List<MessagePart> mockParts = new ArrayList<>();
+
+        //mockMsgPart1 inside original mockMsgPayload
+        MessagePart mockMsgPart1 = new MessagePart();
+        mockMsgPart1.setMimeType("multipart/alternative");
+        List<MessagePart> mockPartsOfParts = new ArrayList<>();
+
+        //mockMsgPart2 inside mockMsgPart1 mockMsgPayload
+        MessagePart mockMsgPart2 = new MessagePart();
+        MessagePartBody mockBody = new MessagePartBody();
+        mockMsgPart2.setMimeType("text/plain");
+        //base64URL encoded String for "text/plain part in multipart/alternative inside multipart/mixed"
+        String mockData =
+                "dGV4dC9wbGFpbiBwYXJ0IGluIG11bHRpcGFydC9hbHRlcm5hdGl2ZSBpbnNpZGUgbXVsdGlwYXJ0L21peGVk";
+
+        mockBody.setData(mockData);
+        mockMsgPart2.setBody(mockBody);             //setting body for text/plain mockMsgPart
+
+        mockParts.add(mockMsgPart1);
+        mockPartsOfParts.add(mockMsgPart2);
+        mockMsgPayload.setParts(mockParts);
+        mockMsgPart1.setParts(mockPartsOfParts);
+        mockMsg.setPayload(mockMsgPayload);
+
+        String result = emailBodyFetcher.getEmailBody(mockMsgId);
+        Assertions.assertEquals(result,"text/plain part in multipart/alternative inside multipart/mixed");
     }
 }
